2159863f2e6f4bf51711c7e93db54223287187b5
Alyxia Sother <alyxia@riseup.net>
2023-03-14 13:59:34 +0100

    [src/App.scss] Some sane standards

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById('app') as HTMLElement);
 root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
