ec575a9a94aa15b647641b33ab6e1d76d928e46f
Alyxia Sother <alyxia@riseup.net>
2023-03-20 15:20:37 +0100

    [components/Slide] Resolve TODOs

--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -1,8 +1,4 @@
-// TODO: Just use one damn stylesheet.
-// TODO: Stop using array indexing for selecting classnames.
 import styles from './slides/styles.module.scss';
-import stylesText from './slides/text.module.scss';
-import stylesTextMedia from './slides/textmedia.module.scss';
 
 interface SlideProps {
   type: string;
@@ -22,14 +18,14 @@ export default function Slide({
 }: SlideProps): JSX.Element {
   return (
     <div className={styles.slide} style={{ backgroundColor: backgroundColour }}>
-      <div className={styles['spacer-vert']}></div>
-      <div className={stylesText.inner}>
-        <div className={styles['spacer-hor']}></div>
-        <div className={stylesText['text-content']} style={{ color: textColour }}>
+      <div className={styles.spacerVert}></div>
+      <div className={styles.inner}>
+        <div className={styles.spacerHor}></div>
+        <div className={styles.textContent} style={{ color: textColour }}>
           {text} {/*(type: {type}) ({mediaUrl})*/}
         </div>
       </div>
-      <div className={styles['spacer-vert']}></div>
+      <div className={styles.spacerVert}></div>
     </div>
   );
 }
